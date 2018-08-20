d736de1d9caa3ed2b78d0ebfe58f6fc81665e64e
Julia Snyder <julesanne493@gmail.com>
2018-08-20 12:50:43 -0400

    julia and malvika hard work

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Patrick from './components/Patrick';
 import Signup from './components/Auth/Signup';
-//import Login from './components/Auth/Login';
+import Login from './components/Auth/Login';
 
 const App = () => (
   <Router>
@@ -10,6 +10,7 @@ const App = () => (
       <Switch>
         <Route exact path='/signup' component={Signup} />
         <Route exact path='/' component={Patrick} />
+        <Route exact path="/login" component={Login} />
         {/* <Route exact path='/login' component={Login} /> */}
         {/* <Route component={NoMatch} /> */}
       </Switch>
