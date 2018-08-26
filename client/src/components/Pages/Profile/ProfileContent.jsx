0d9abf322902fcaec21157328f70bdf5ab2f296d
Anthony Sin <anthony.t.sin@gmail.com>
2018-08-26 14:15:54 -0400

    Outfits tab complete

--- client/src/components/Pages/Profile/ProfileContent.jsx
+++ client/src/components/Pages/Profile/ProfileContent.jsx
@@ -10,9 +10,23 @@ class ProfileContent extends Component {
         <div className="row">
           {outfitsData && outfitsData.map(outfit => {
             return (
-              <div className="col-12 col-sm-6 col-md-4 mb-4" key={`profileOutfits-${outfit.id}`}>
-                <div>
+              <div className="col-6 col-md-4 mb-4" key={`profileOutfits-${outfit.id}`}>
+                <div className="Profile-outfit">
                   <img className="img-fluid w-100" src={outfit.imageUrl} alt={outfit.id} />
+                  <a href={`/outfits/${outfit.id}`} className="Profile-outfit-overlay">
+                    <div className="row no-gutters w-100">
+                      <div className="col-12 col-sm text-center text-sm-right likes-info">
+                        <span>
+                          <i className="fas fa-heart"></i> {outfit.Likes.length}
+                        </span>
+                      </div>
+                      <div className="col-12 col-sm text-center text-sm-left comments-info">
+                        <span>
+                          <i className="fas fa-comment"></i> {outfit.Comments.length}
+                        </span>
+                      </div>
+                    </div>
+                  </a>
                 </div>              
               </div>
             );
