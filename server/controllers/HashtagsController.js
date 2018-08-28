3789f7b1450a585c2d34bf46339d8606525629e7
Anthony Sin <anthony.t.sin@gmail.com>
2018-08-27 20:01:54 -0400

    GET /api/hashtags/:text route

---
+++ server/controllers/HashtagsController.js
@@ -0,0 +1,38 @@
+const { Hashtag, Outfit, Category, Like, Comment } = require('../models');
+
+const getHashtagOutfits = (req, res) => {
+  (async() => {
+    const hashtagWithOutfits = await Hashtag.findOne({
+      where: {
+        text: req.params.text,
+      },
+      attributes: ['text'],
+      include: [
+        {
+          model: Outfit,
+          attributes: { exclude: ['description', 'updatedAt'] },
+          include: [
+            {
+              model: Category,
+              attributes: ['id', 'name'],
+            },
+            {
+              model: Like,
+              attributes: ['id'],
+            },
+            { 
+              model: Comment,
+              attributes: ['id'],
+            }
+          ],
+        },
+      ]
+    });
+
+    res.json(hashtagWithOutfits);
+  })();
+}
+
+module.exports = {
+  getHashtagOutfits
+};
